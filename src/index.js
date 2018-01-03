3cbda6df831e62088edd6d52b4b5bd433d23db04
Esenor <daviddieu80@gmail.com>
2018-01-03 12:07:59 +0100

    Switch port 3000 to 3042

--- src/index.js
+++ src/index.js
@@ -4,14 +4,32 @@ const { buildSchema } = require('graphql')
 const SchemaParser = require('./SchemaParser')
 const Resolver = require('./Resolver')
 const devTool = require('./devTool')
+const cors = require('cors')
 
 const schema = SchemaParser.getSchema()
+const corsOptions = {
+  origin(origin, callback) {
+    callback(null, true);
+  },
+  credentials: true
+}
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type,token')
+  next()
+}
+
 /**
  * Create an Express application
  * @return {Express}
  */
 function createApplication () {
   let application = express()
+
+  application.use(cors(corsOptions))
+  application.use(allowCrossDomain)
+
   application.use('/', graphqlHTTP({
     // Load GQL Schema
     schema: buildSchema(schema.toString()),
@@ -28,10 +46,10 @@ function createApplication () {
  * @return {Server}
  */
 function startServer (application, port) {
-  console.log(`***************\n*** GRAPHQL ***\n\nStarted on http://localhost:3000\n----------\nSchemas availables\n${schema}`)
+  console.log(`***************\n*** GRAPHQL ***\n\nStarted on http://localhost:${port}\n----------\nSchemas availables\n${schema}`)
   return application.listen(port)
 }
 
 // Start application
-startServer(createApplication(), 3000)
+startServer(createApplication(), 3042)
 devTool.addSampleCustomer()
